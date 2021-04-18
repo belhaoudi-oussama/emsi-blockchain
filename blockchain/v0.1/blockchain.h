bb73137f749b325067a916fe66f9c92c8ffa3e8c
belhaoudi-oussama <belhaoudi.oussama@gmail.com>
2021-04-18 15:04:00 +0000

    Task 6 done serialize the blocks into a file

--- blockchain/v0.1/blockchain.h
+++ blockchain/v0.1/blockchain.h
@@ -31,6 +31,12 @@
 #define GENESIS_INIT_HASH \
 	"\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d"\
 	"\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03"
+
+#define HBLK_MAGIC "HBLK"
+#define HBLK_VERSION "0.1"
+#define HBLK_MAGIC_LEN 4
+#define HBLK_VERSION_LEN 3
+
 #define GENESIS_INIT_BLOCK { \
 		{ /* info */	    \
 				GENESIS_INDEX /* index */,		\
@@ -111,10 +117,21 @@ typedef struct blockchain_s
     llist_t     *chain;
 } blockchain_t;
 
+typedef struct header_s
+{
+	uint8_t	hblk_magic[HBLK_MAGIC_LEN]; 
+	uint8_t	hblk_version[HBLK_VERSION_LEN]; 
+	uint8_t	hblk_endian;
+	int32_t	hblk_blocks;
+
+} header_t;
+
+
 blockchain_t *blockchain_create(void);
 block_t *block_create(block_t const *prev, int8_t const *data, uint32_t data_len);
 void block_destroy(block_t *block);
 void blockchain_destroy(blockchain_t *blockchain);
 uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH]);
+int blockchain_serialize(blockchain_t const *blockchain, char const *path);
 
 #endif
